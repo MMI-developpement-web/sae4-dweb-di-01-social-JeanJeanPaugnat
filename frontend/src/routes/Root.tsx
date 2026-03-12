9d858eb349964f334dfa6881c80694edcfc73883
Jean Paugnat <poprixindustrie@gmail.com>
2026-03-12 17:46:34 +0000

    root route

---
+++ frontend/src/routes/Root.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import App from "../App";
+
+export default function Root() {
+
+  return (
+    <>
+        <App />
+        <Outlet />
+    </>
+  );
+}
