8cec16026892c0c35d6f51a0c7f06229154726ff
Jean Paugnat <poprixindustrie@gmail.com>
2026-03-17 09:21:24 +0000

    entity user migrated

---
+++ backend/src/Entity/Post.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Content = null;
+
+    #[ORM\Column]
+    private ?\DateTime $Date_creation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->Content;
+    }
+
+    public function setContent(string $Content): static
+    {
+        $this->Content = $Content;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTime
+    {
+        return $this->Date_creation;
+    }
+
+    public function setDateCreation(\DateTime $Date_creation): static
+    {
+        $this->Date_creation = $Date_creation;
+
+        return $this;
+    }
+}
