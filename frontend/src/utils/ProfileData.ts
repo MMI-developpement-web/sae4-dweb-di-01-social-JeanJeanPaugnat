29c7e4f6f52ed36b7f0a055622d9f3efd72ecbdc
Jean Paugnat <poprixindustrie@gmail.com>
2026-03-22 22:28:28 +0000

    showing public profile function

--- frontend/src/utils/ProfileData.ts
+++ frontend/src/utils/ProfileData.ts
@@ -24,4 +24,29 @@ let showMyProfile = async function() {
     return data;
 };
 
-export { showMyProfile };
+let showPublicProfile = async function({ params }: any) {
+    let username = params.username;
+    let token = localStorage.getItem('mon_token'); 
+    
+    let headers: HeadersInit = {
+        "Content-Type": "application/json",
+    };
+    if (token) {
+        headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    let response = await fetch(`${API_URL}/profile/${username}`, {
+        method: "GET",
+        headers: headers,
+    });
+
+    if (!response.ok) {
+        console.error("Failed to fetch public profile data");
+        return;
+    }
+
+    let data = await response.json();
+    return data;
+};
+
+export { showMyProfile, showPublicProfile };
