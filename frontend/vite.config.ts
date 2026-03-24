a008389ba7f87d5f24b5f8ee9512b9b27395cf29
Jean Paugnat <poprixindustrie@gmail.com>
2026-03-24 15:01:30 +0000

    remove console.log on build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,36 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+function removeConsoleLog() {
+  return {
+    name: 'remove-console-log',
+    enforce: 'post' as const,
+    transform(code :any, id :any) {
+      if (process.env.NODE_ENV === 'production' && /\\.[jt]sx?$/.test(id)) {
+        return {
+          code: code.replace(/console\\.log\\([^;]*\\);?/g, ''),
+          map: null,
+        };
+      }
+      return null;
+    },
+  };
+}
+
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), removeConsoleLog()],
   preview: {
-   port: 5173,
-   strictPort: true,
+    port: 5173,
+    strictPort: true,
   },
   server: {
-   port: 5173,
-   strictPort: true,
-   host: true,
-   origin: "http://localhost:8090",
-   allowedHosts: ["sae-frontend"]
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://localhost:8090",
+    allowedHosts: ["sae-frontend"]
   },
 });
