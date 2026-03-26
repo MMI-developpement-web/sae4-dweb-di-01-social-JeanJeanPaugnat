1be8c6ffd7037574ccef089f52d25e9810071222
JeanJeanPaugnat <poprixindustrie@gmail.com>
2026-03-26 17:57:39 +0100

    verify is blocked user on the token sent

--- backend/src/Security/AccessTokenHandler.php
+++ backend/src/Security/AccessTokenHandler.php
@@ -7,6 +7,7 @@ use App\Repository\AccessTokenRepository; // Assurez-vous d'avoir bien créé vo
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Http\AccessToken\AccessTokenHandlerInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 class AccessTokenHandler implements AccessTokenHandlerInterface
 {
@@ -28,6 +29,8 @@ class AccessTokenHandler implements AccessTokenHandlerInterface
             throw new BadCredentialsException('Invalid token.');
         }
 
+
+
         // Si vous avez hashé le token en base, il faudrait le hacher ici avant de le comparer.
         // if ($hashedToken !== $token->getValue()) { ... } 
         // Mais pour l'instant, on suppose que le token est stocké en clair (comme l'énoncé)
@@ -35,6 +38,9 @@ class AccessTokenHandler implements AccessTokenHandlerInterface
         // 2. Récupérer l'utilisateur
         $user = $token->getUser();
         
+        if (!$user || $user->getIsBlocked()) {
+            throw new CustomUserMessageAuthenticationException('Votre compte a été suspendu pour non-respect des conditions.');
+        }
         // 3. Retourner un UserBadge avec l'identifiant de l'utilisateur.
         // !!! IMPORTANT : Utilisez la propriété qui identifie l'utilisateur dans votre provider (généralement l'email)
         return new UserBadge($user->getEmail());
