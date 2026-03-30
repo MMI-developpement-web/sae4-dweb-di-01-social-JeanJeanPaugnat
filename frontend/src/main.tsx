a303b191566af47fa087eb0bc6db29c9feeda368
Jean Paugnat <poprixindustrie@gmail.com>
2026-03-30 14:21:25 +0000

    route created for editpost

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LogIn from './routes/LogIn.tsx';
 import SignUp from './routes/SignUp.tsx';
 import CreatePost from './routes/CreatePost.tsx';
+import EditPostPage from './routes/EditPost.tsx';
 import Feed from './routes/Feed.tsx';
 import Home from './routes/Home.tsx';
 
@@ -13,6 +14,7 @@ import Navbar from './components/ui/Navbar.tsx';
 import ProfilePage from './routes/ProfilePage.tsx';
 import EditProfilePage from './routes/EditProfilePage.tsx';
 import { showPublicProfile, showMyProfile } from './utils/ProfileData.ts';
+import { getPost } from './utils/PostData.ts';
 // import './index.css';
 
 
@@ -36,6 +38,13 @@ const router = createBrowserRouter([
   {
     path: '/feed',
     element: <Feed />,
+  },
+  {
+    path: '/edit-post/:postId',
+    element: <EditPostPage />,
+    loader: async ({ params }) => {
+      return getPost(Number(params.postId));
+    },
   },
   {
     path: '/profile/edit',
