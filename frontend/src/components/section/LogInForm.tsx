22d2d8cfdd931a1672031ca5dac3c72ca4923860
Jean Paugnat <poprixindustrie@gmail.com>
2026-04-01 12:19:18 +0000

    useState instead of querySelector

--- frontend/src/components/section/LogInForm.tsx
+++ frontend/src/components/section/LogInForm.tsx
@@ -9,29 +9,20 @@ import { useNavigate, Link } from "react-router-dom";
 export default function LogInForm() {
       const [email, setEmail] = useState("");
       const [password, setPassword] = useState("");
+      const [errorMessage, setErrorMessage] = useState("");
       const navigate = useNavigate();
 
       let handleLogin = async (email: string, password: string) => {
           let data = await Login(email, password);
-          //  catch eror envoyé par le backend si error de email ou mot de passe, afficher message d'erreur activer la div error_message
-              if (data && data.error) {
+          if (data && data.error) {
                 console.log(data.error);
-                  const errorMessageDiv = document.getElementById("error_message");
-                  if (errorMessageDiv) {
-                      errorMessageDiv.classList.remove("hidden");
-                      const errorText = errorMessageDiv.querySelector("#error_text");
-                      if (errorText) {
-                          errorText.textContent = data.error;
-                      }
-                  }
-                  return;
-              }
+                setErrorMessage(data.error);
+                return;
+          }
           if (data) {
               console.log("Login successful:", data);
-      
               navigate("/feed");
           }
-      
       }
 
     return (
@@ -41,10 +32,12 @@ export default function LogInForm() {
         onChange={e => setEmail(e.target.value)}/>
             <FormField label="Password"  placeholder="Enter your password" action="password" value={password}
         onChange={e => setPassword(e.target.value)}/>
-            <div id="error_message" className="hidden flex flex-row items-center justify-center gap-[8px] text10-regular ">
-                <Icon nameIcon="circle-alert" size={18} color='#F22D2D' />
-                <span id="error_text" className="text-red-warning text12-regular"></span>
-            </div>
+            {errorMessage && (
+                <div className="flex flex-row items-center justify-center gap-[8px] text10-regular">
+                    <Icon nameIcon="circle-alert" size={18} color='#F22D2D' />
+                    <span className="text-red-warning text12-regular">{errorMessage}</span>
+                </div>
+            )}
             <div className="flex flex-row gap-2.5 text10-regular items-center justify-center">
                 <span className="text-light-text">Don't have an account?</span>
                 <Link to="/signup" className="text-blue-800 ">Sign Up</Link>
